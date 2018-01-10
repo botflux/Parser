6ae7c16e4a88448cb57cbd590fbd7ea816e3a18a
botflux <victor.mendele68@gmail.com>
2018-01-10 15:23:58 +0100

    Add files via upload

---
+++ Parser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parsed = MessageParser.Parse("rouge", "100");
+
+            Console.WriteLine(parsed);
+            
+            MessageParser.ParsedData data = MessageParser.Decode(parsed);
+            Console.WriteLine(data.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
